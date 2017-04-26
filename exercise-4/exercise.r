16a20607ac431b45ef53b9744d13304fc26137d3
amikataria <amiteshkataria@D-69-91-157-19.dhcp4.washington.edu>
2017-04-26 15:09:21 -0700

    worked on exercise 4

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -4,15 +4,16 @@ library(httr)
 
 # Make a variable base.url that has the same base url from the omdb documentation.
 # (Hint: visit https://www.omdbapi.com/ to find the base url)
+base.uri <- "https://www.omdbapi.com"
 
 
 # Make a variable called movie that has the name of your favorite movie
-
+movie <- "Logan"
 
 # Make a variable called "query.parameters" that holds a list of the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available.
-
+query.parameters <- t
 
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
